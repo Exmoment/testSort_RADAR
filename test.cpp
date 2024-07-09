b35e8025df5cde66ef597d807cea1cba84365813
Exmoment <139344527+Exmoment@users.noreply.github.com>
2024-07-09 22:03:10 +0300

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -125,9 +125,30 @@ void createArr(){
 }
 
 void readArr(){
-    bool var = true;
-    int coutRows = 0;
+    int y = 2, a, k, N;
+    int numb = 0;                                               //счетчик переменных по индексу Y, необходим для исключения N-ых строк
+    int countRows = 0;
     string line, pathToFile;
+    
+    cout << "Enter N: ";
+    while(true){
+        cin >> N;
+        if(N >= 2){//параметр для исключения каждой N-ой строки
+            if(cin.fail()) {
+                cin.clear();
+                cin.ignore();
+                cout << "Not num" << endl;
+                cout << "Enter N: ";
+            } else {
+                break;
+            }
+        } else{
+            cin.clear();
+            cin.ignore();
+            cout << "N must be > 2" << endl;
+            cout << "Enter N: ";
+        }
+    }
     
     ifstream inputFile;
     while(true){
@@ -137,17 +158,65 @@ void readArr(){
         if(inputFile.is_open()){
             while(!inputFile.eof()){
                 getline(inputFile, line);
-                cout<< line << endl;
-                coutRows++;
+                countRows++;
             }
             inputFile.close();
-            int arr[coutRows][2];
             break;
         } else{
             cout<< "Error. File not found> try again" << endl;
         }
     }
+    int arr[countRows][y];
+    int newArr[countRows][y];
+    inputFile.open(pathToFile);
+    for(int i = 0; i < countRows; i++){
+        for(int j = 0; j < y; j++){
+            inputFile >> arr[i][j];
+        }
+    }
     
+    for(int i = 0; i < countRows; i++){
+        if(arr[i][1] == arr[i+1][1]){
+            if(numb == (N - 1)){
+               cout << "Row " << i+1 << " is deleted" << endl;
+            } else{
+                newArr[a][0] = arr[i][0];
+                newArr[a][1] = arr[i][1];
+                a++;
+            }
+            numb++;
+            } else{
+                numb = 0;
+                newArr[a][0] = arr[i][0];
+                newArr[a][1] = arr[i][1];
+                a++;
+            }
+        }
+        
+    while(a < countRows){
+        for(int j = 0; j < y; j++){
+            newArr[a][j] = 00;
+        }
+        a++;
+        k++;
+    }
+    
+    ofstream resultFile;                                       //вывод в файле результирующего массива данных
+    resultFile.open("result.txt", ios_base::out);
+    if (resultFile.is_open()){
+
+        for(int i = 0; i < (a - k); i++){
+            for(int j = 0; j < y; j++){
+                
+                if(j == 1){
+                    resultFile << newArr[i][j] << endl;
+                } else {
+                    resultFile << newArr[i][j] << " ";
+                }
+            }
+        }
+    }
+    resultFile.close();
 }
 
 int main(){
